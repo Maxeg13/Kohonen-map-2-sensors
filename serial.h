37d272730ded0341a2aba8b8ba90a68a7b01a149
Maxeg13 <chibisaincrada@gmail.com>
2017-07-09 10:47:23 +0300

    create

---
+++ serial.h
@@ -0,0 +1,15 @@
+//modified from http://blablacode.ru/programmirovanie/392
+#ifndef SERIAL_DLL
+#define SERIAL_DLL
+#include "stdafx.h"
+#include "targetver.h"
+#include <Windows.h>
+class Serial
+{
+public:
+    HANDLE hSerial;
+void  InitCOM(LPCTSTR );
+char  ReadCOM(bool& );
+
+};
+#endif // SERIAL_H
